3af0d55ce74f04178e059be0aed4941aa1ae5c08
Christian Baños <christiandepaulbp@gmail.com>
2024-12-18 12:34:41 +0000

    añadiendo las bebidas a la mini base de datos

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Inventory
 # - crear boton de busqueda / done
-# - añadir todas las bebidas con su categoria / 
+# - añadir todas las bebidas con su categoria / Faltan:
+# Mixers - Juice - Pure - Fruits - Coffee
 # - introducir la logica para el boton de busqueda /
--- script.js
+++ script.js
@@ -1,11 +1,97 @@
 // Inventario de bebidas
 const inventory = [
-  { name: "Coca Cola", category: "Soft drink" },
-  { name: "Fanta", category: "Soft drink" },
-  { name: "Sprite", category: "Soft drink" },
-  { name: "Agua", category: "Soft drink" },
-  { name: "Vino", category: "Alcoholic" },
-  { name: "Cerveza", category: "Alcoholic" },
+  { name: "Roe & Co", category: 'Irish whiskey' },
+  { name: "Bushmills", category: 'Irish whiskey' },
+  { name: "Bushmills Malt 10YO", category: 'Irish whiskey' },
+  { name: "Bushmills Malt 16YO", category: 'Irish whiskey' },
+  { name: "Blackbush", category: 'Irish whiskey' },
+  { name: "Jameson", category: 'Irish whiskey' },
+  { name: "Jameson Crested", category: 'Irish whiskey' },
+  { name: "Jameson Coppers Croze", category: 'Irish whiskey' },
+  { name: "Jameson Distillers Croze", category: 'Irish whiskey' },
+  { name: "Jameson Distillers Safe", category: 'Irish whiskey' },
+  { name: "Yellow Spot", category: 'Irish whiskey' },
+  { name: "Green Spot", category: 'Irish whiskey' },
+  { name: "Powers", category: 'Irish whiskey' },
+  { name: "Paddy", category: 'Irish whiskey' },
+  { name: "Tullamore Dew", category: 'Irish whiskey' },
+  { name: "Redbreast 12YO", category: 'Irish whiskey' },
+  { name: "Redbreast 15YO", category: 'Irish whiskey' },
+  { name: "Jack Daniels", category: 'Bourbon' },
+  { name: "Jim Beam", category: 'Bourbon' },
+  { name: "Bulleit", category: 'Bourbon' },
+  { name: "Southern Comfort", category: 'Bourbon' },
+  { name: "Makers Mark", category: 'Bourbon' },
+  { name: "Johnnie Walker Red", category: 'Scotch' },
+  { name: "Jhonnie Walker Black 12Y", category: 'Scotch' },
+  { name: "Chivas Regal", category: 'Scotch' },
+  { name: "Cutty Shark", category: 'Scotch' },
+  { name: "Teachers", category: 'Scotch' },
+  { name: "Hennesy", category: 'Brandy' },
+  { name: "Bacardi", category: 'Rum' },
+  { name: "Captain Morgan", category: 'Rum' },
+  { name: "The Kraken", category: 'Rum' },
+  { name: "Sea Dog", category: 'Rum' },
+  { name: "Smirnoff", category: 'Vodka' },
+  { name: "Absolute", category: 'Vodka' },
+  { name: "Absolute Vanilla", category: 'Vodka' },
+  { name: "Absolute Raspberry", category: 'Vodka' },
+  { name: "Ciroc", category: 'Vodka' },
+  { name: "Ketel One", category: 'Vodka' },
+  { name: "Grey Goose", category: 'Vodka' },
+  { name: "Gordons", category: 'Gin' },
+  { name: "Gordons Pink", category: 'Gin' },
+  { name: "Gordons 0.0", category: 'Gin' },
+  { name: "Boe Violet", category: 'Gin' },
+  { name: "Cork Dry", category: 'Gin' },
+  { name: "Tanqueray", category: 'Gin' },
+  { name: "Tanqueray London Nº10", category: 'Gin' },
+  { name: "Hendricks", category: 'Gin' },
+  { name: "Brockmans", category: 'Gin' },
+  { name: "Bombay Sapphire", category: 'Gin' },
+  { name: "Bombay East", category: 'Gin' },
+  { name: "Dingle", category: 'Gin' },
+  { name: "Gunpowder", category: 'Gin' },
+  { name: "Beefeater", category: 'Gin' },
+  { name: "Baileys", category: 'Liquors' },
+  { name: "Grand Marnier", category: 'Liquors' },
+  { name: "Irish Mist", category: 'Liquors' },
+  { name: "Creme de Menthe", category: 'Liquors' },
+  { name: "Peach Schnapps", category: 'Liquors' },
+  { name: "Tia Maria", category: 'Liquors' },
+  { name: "Kahlua", category: 'Liquors' },
+  { name: "Drambuie", category: 'Liquors' },
+  { name: "Cointreau", category: 'Liquors' },
+  { name: "Jagermeister", category: 'Liquors' },
+  { name: "Amaretto", category: 'Liquors' },
+  { name: "Creme de Cassis", category: 'Liquors' },
+  { name: "Sambuca", category: 'Liquors' },
+  { name: "Tequila Siver", category: 'Tequila' },
+  { name: "Tequila Rose", category: 'Tequila' },
+  { name: "Pernod", category: 'Aperitifs' },
+  { name: "Grand Marnier", category: 'Aperitifs' },
+  { name: "Dubonnet", category: 'Aperitifs' },
+  { name: "Martini", category: 'Aperitifs' },
+  { name: "Bristol Cream", category: 'Aperitifs' },
+  { name: "Tio Pepe", category: 'Aperitifs' },
+  { name: "Pimms", category: 'Aperitifs' },
+  { name: "Churchill", category: 'Aperitifs' },
+  { name: "Budwiser", category: 'Boottles' },
+  { name: "Heineken", category: 'Boottles' },
+  { name: "Peroni", category: 'Boottles' },
+  { name: "Coors", category: 'Boottles' },
+  { name: "Corona", category: 'Boottles' },
+  { name: "Rocshore", category: 'Boottles' },
+  { name: "Kopparberg strawberry & lime", category: 'Boottles' },
+  { name: "Kopparberg Mixed Fruit", category: 'Boottles' },
+  { name: "Bulmers Original", category: 'Boottles' },
+  { name: "Bulmers Light", category: 'Boottles' },
+  { name: "WKD", category: 'Alcopops' },
+  { name: "Smirnoff ice", category: 'Alcopops' },
+  { name: "West Coast Cooler Original", category: 'Alcopops' },
+  { name: "West Coast Cooler Rose", category: 'Alcopops' },
+  { name: "", category: '' },
+
 ];
 
 // Referencias a elementos del DOM
@@ -29,13 +115,15 @@ function renderTable(categoryFilter = "all") {
     row.innerHTML = `
           <td>${item.name}</td>
           <td>
-            <input type="number" id="needed-${index}" min="0" placeholder="0">
+            <input type="number" id="needed-${index}" min="0" placeholder="0" class="input_needed">
           </td>
           <td>${item.category}</td>
         `;
     inventoryTable.appendChild(row);
   });
 }
+
+
 
 // Inicializar la tabla al cargar la página
 renderTable();
